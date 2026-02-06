de63d2fab1a94d0306bd2fc110c862a2f208ec07
ErenDurham <erendurham@gmail.com>
2026-02-05 23:37:25 -0500

    comments talking about ALU

--- ALU.v
+++ ALU.v
@@ -9,6 +9,32 @@ module ALU (
 );
     // Illegal operators
     // * , / , + , - , << , >> , <<< , >>>
+    // so only logic gates and no shifting
+
+    /*
+        1. imm or reg? decoder (based on op code)
+        2. implement LCA (look ahead carry adder) -> Dawn shall do this
+            - This handles Add + Sub (need two's compliment)
+        3. bit shifting (shift left and shift right) -> Eren....barrel shifting (w/o clock)
+        4. Logical (does not preserve sign) + arithmetic (preseves sign)
+            OR (Eren), AND (Dawn), NOT, XOR, Compariter
+        5. Multiplexer
+
+
+        Operations that ALU needs to perform
+        Arithmetic Operations:
+            1-	Addition.
+            2-	Subtraction.
+        Logic Operations:
+            1-	OR Operation
+            2-	AND Operation
+            3-	XOR Operation
+            4-	Left Shift Operation
+            5-	Right Shift Operation.
+            6-	Complement.
+    */
+
+
 
     // function multiplexer
     // Use to select the operation based on iFunct3 and iFunct7
